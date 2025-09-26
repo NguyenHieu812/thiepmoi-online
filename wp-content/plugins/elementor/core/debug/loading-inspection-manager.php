f5c8edbf89cabd084865feeef4f3a0d6c9007cd1
duongthai2003 <thaiq9577@gmail.com>
2025-09-26 22:18:24 +0700

    uninstall plugin don't úe

--- wp-content/plugins/elementor/core/debug/loading-inspection-manager.php
+++ wp-content/plugins/elementor/core/debug/loading-inspection-manager.php
@@ -1,18 +1,20 @@
 <?php
+
 namespace Elementor\Core\Debug;
 
 use Elementor\Core\Debug\Classes\Inspection_Base;
 use Elementor\Core\Debug\Classes\Shop_Page_Edit;
 use Elementor\Core\Debug\Classes\Theme_Missing;
-use Elementor\Core\Debug\Classes\Htaccess;
 use Elementor\Utils;
 
-class Loading_Inspection_Manager {
+class Loading_Inspection_Manager
+{
 
 	public static $_instance = null;
 
-	public static function instance() {
-		if ( null === self::$_instance ) {
+	public static function instance()
+	{
+		if (null === self::$_instance) {
 			self::$_instance = new Loading_Inspection_Manager();
 		}
 		return self::$_instance;
@@ -21,12 +23,12 @@ class Loading_Inspection_Manager {
 	/** @var Inspection_Base[] */
 	private $inspections = [];
 
-	public function register_inspections() {
+	public function register_inspections()
+	{
 		$this->inspections['theme-missing'] = new Theme_Missing();
-		$this->inspections['htaccess'] = new Htaccess();
 
-		$is_editing_shop_page = Utils::get_super_global_value( $_GET, 'post' ) == get_option( 'woocommerce_shop_page_id' );
-		if ( $is_editing_shop_page ) {
+		$is_editing_shop_page = Utils::get_super_global_value($_GET, 'post') == get_option('woocommerce_shop_page_id');
+		if ($is_editing_shop_page) {
 			$this->inspections['shop-page-edit'] = new Shop_Page_Edit();
 		}
 	}
@@ -34,18 +36,20 @@ class Loading_Inspection_Manager {
 	/**
 	 * @param Inspection_Base $inspection
 	 */
-	public function register_inspection( $inspection ) {
-		$this->inspections[ $inspection->get_name() ] = $inspection;
+	public function register_inspection($inspection)
+	{
+		$this->inspections[$inspection->get_name()] = $inspection;
 	}
 
-	public function run_inspections() {
+	public function run_inspections()
+	{
 		$debug_data = [
-			'message' => esc_html__( "We’re sorry, but something went wrong. Click on 'Learn more' and follow each of the steps to quickly solve it.", 'elementor' ),
-			'header' => esc_html__( 'The preview could not be loaded', 'elementor' ),
+			'message' => esc_html__("We’re sorry, but something went wrong. Click on 'Learn more' and follow each of the steps to quickly solve it.", 'elementor'),
+			'header' => esc_html__('The preview could not be loaded', 'elementor'),
 			'doc_url' => 'https://go.elementor.com/preview-not-loaded/',
 		];
-		foreach ( $this->inspections as $inspection ) {
-			if ( ! $inspection->run() ) {
+		foreach ($this->inspections as $inspection) {
+			if (! $inspection->run()) {
 				$debug_data = [
 					'message' => $inspection->get_message(),
 					'header' => $inspection->get_header_message(),
